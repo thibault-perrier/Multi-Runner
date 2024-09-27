226d9d6a60c91228f371d18e4ed145b00f2ee718
thibault-perrier <thibaultperrier764@gmail.com>
2024-09-27 14:12:56 +0200

    init mp
    create and join by session code
    - bug on networkBehaviour not found in UIOnJoin

---
+++ Multi-Runner/Assets/Script/Network/UIOnJoin.cs
@@ -0,0 +1,18 @@
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class UIOnJoin : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
